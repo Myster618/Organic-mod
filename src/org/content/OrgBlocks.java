9fa44c28b3e8cd0189c78ba13e3228906e5a4a8d
Myster618 <74233076+Myster618@users.noreply.github.com>
2023-01-29 21:47:41 +0300

    Create OrgBlocks.java

---
+++ src/org/content/OrgBlocks.java
@@ -0,0 +1,32 @@
+package org.content
+
+import arc.graphics.*;
+import arc.math.*;
+import arc.struct.*;
+import org.*;
+import mindustry.content.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+
+
+public class OrgBlocks {
+  public static Block
+    //environment
+    calcite, calcite-wall-ore, carbonalt-floor, carbonalt-stone, carbon-ore, chitin, deep-blood,
+    energia-crater, energia-puddle, flesh-floor, flesh-wall, plant-floor, plant-stone, 
+    shallow-blood,
+    //decorations
+    boulder-green, boulder-red, boulder-black, sprout-light-green, sprout-dark-green;
+}
