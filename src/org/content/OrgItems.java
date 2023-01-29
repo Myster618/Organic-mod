d4c301508ce4edd19dfdb4b617d98e4897c7dc41
Myster618 <74233076+Myster618@users.noreply.github.com>
2023-01-29 12:25:39 +0300

    Create OrgItems.java

---
+++ src/org/content/OrgItems.java
@@ -0,0 +1,50 @@
+package org.content;
+
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+import static mindustry.content.Items.*;
+
+public class OrgItems {
+  public static Item calcium, carbon, chitin, phosphorus, resin, tissue;
+  
+  public static final Seq<Item> termanphettItems = new Seq<>();
+  
+  public static void load() {
+    calcium  = new Item("calcium", Color.valueOf("ffffff")) {{
+      hardness = 1;
+      alwaysUnlocked = true;
+    }};
+    
+    carbon = new Item("carbon", Color.valueOf("3c3838")) {{
+      hardness = 0;
+      alwaysUnlocked = true;
+    }};
+    
+    chitin = new Item("chitin", Color.valueOf("ebeef5")) {{
+      hardness = 3;
+    }};
+    
+    phosphorus = new Item("phosphorus", Color.valueOf("742222")) {{
+      hardness = 2;
+      flammability = 1.5;
+      explosiveness = 0.0005;
+    }};
+                          
+    resin = new Item("resin", Color.valueOf("eac629")) {{
+      flammability = 0.05;
+      charge = 0.2;
+    }};
+    
+    tissue = new Item("tissue", Color.valueOf("999781")) {{
+      flammability = 0.2;
+      hardness = 2;
+    }};
+    
+    
+    termanphettItems.addAll(
+          calcium, carbon, chitin, phosphorus, resin, tissue
+    );
+  }
+}
