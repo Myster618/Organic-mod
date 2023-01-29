cfaa21fe63b04806ce5f14dc702d32dca6e7baea
Myster618 <74233076+Myster618@users.noreply.github.com>
2023-01-29 18:36:12 +0300

    Update OrgItems.java

--- src/org/content/OrgItems.java
+++ src/org/content/OrgItems.java
@@ -18,27 +18,27 @@ public class OrgItems {
       alwaysUnlocked = true;
     }};
     
-    carbon = new Item("carbon", Color.valueOf("3c3838") {{
+    carbon = new Item("carbon", Color.valueOf("3c3838")) {{
       hardness = 0;
       alwaysUnlocked = true;
     }};
     
-    chitin = new Item("chitin", Color.valueOf("ebeef5") {{
+    chitin = new Item("chitin", Color.valueOf("ebeef5")) {{
       hardness = 3;
     }};
     
-    phosphorus = new Item("phosphorus", Color.valueOf("742222") {{
+    phosphorus = new Item("phosphorus", Color.valueOf("742222")) {{
       hardness = 2;
       flammability = 1.5f;
       explosiveness = 0.0005f;
     }};
                           
-    resin = new Item("resin", Color.valueOf("eac629") {{
+    resin = new Item("resin", Color.valueOf("eac629")) {{
       flammability = 0.05f;
       charge = 0.2f;
     }};
     
-    tissue = new Item("tissue", Color.valueOf("999781") {{
+    tissue = new Item("tissue", Color.valueOf("999781")) {{
       flammability = 0.2f;
       hardness = 2;
     }};
