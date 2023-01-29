a237b9c06584ff0f43f804b8c0b6fb20281a8368
Myster618 <74233076+Myster618@users.noreply.github.com>
2023-01-29 18:28:04 +0300

    Update OrgItems.java

--- src/org/content/OrgItems.java
+++ src/org/content/OrgItems.java
@@ -13,34 +13,38 @@ public class OrgItems {
   public static Item calcium, carbon, chitin, phosphorus, resin, tissue;
   
   public static void load() {
-    calcium  = new Item("calcium", Color.valueOf("ffffff")) {{
+    calcium  = new Item("calcium", Color.valueOf("ffffff")){{
       hardness = 1;
       alwaysUnlocked = true;
     }};
     
-    carbon = new Item("carbon", Color.valueOf("3c3838")) {{
+    carbon = new Item("carbon", Color.valueOf("3c3838"){{
       hardness = 0;
       alwaysUnlocked = true;
     }};
     
-    chitin = new Item("chitin", Color.valueOf("ebeef5")) {{
+    chitin = new Item("chitin", Color.valueOf("ebeef5"){{
       hardness = 3;
     }};
     
-    phosphorus = new Item("phosphorus", Color.valueOf("742222")) {{
+    phosphorus = new Item("phosphorus", Color.valueOf("742222"){{
       hardness = 2;
       flammability = 1.5f;
       explosiveness = 0.0005f;
     }};
                           
-    resin = new Item("resin", Color.valueOf("eac629")) {{
+    resin = new Item("resin", Color.valueOf("eac629"){{
       flammability = 0.05f;
       charge = 0.2f;
     }};
     
-    tissue = new Item("tissue", Color.valueOf("999781")) {{
+    tissue = new Item("tissue", Color.valueOf("999781"){{
       flammability = 0.2f;
       hardness = 2;
     }};
+    
+    serpuloItems.addAll(
+      calcium, carbon, chitin, phosphorus, resin, tissue
+     );                
   }
 }
