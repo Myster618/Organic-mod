d14afb552ec0744768bca1b15af6beede6286314
Myster618 <74233076+Myster618@users.noreply.github.com>
2023-02-01 19:46:56 +0300

    Update OrgAttribute.java

--- src/org/world/meta/OrgAttribute.java
+++ src/org/world/meta/OrgAttribute.java
@@ -8,10 +8,5 @@ import static mindustry.content.Blocks.*;
 
 public class OrgAttribute {
   public static final Attribute resin = Attribute.add("resin");
-  //adding attribute to vanilla content
-  static {
-    Events.run(ContentInitEvent.class, () -> {
-      sand.attributes.set(resin, 0.2f);
-    });  
   }
 }
