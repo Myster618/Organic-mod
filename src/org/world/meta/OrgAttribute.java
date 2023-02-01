8db19ec11718b27ade11c6c476cf79639f33d01d
Myster618 <74233076+Myster618@users.noreply.github.com>
2023-02-01 20:15:21 +0300

    Update OrgAttribute.java

--- src/org/world/meta/OrgAttribute.java
+++ src/org/world/meta/OrgAttribute.java
@@ -3,8 +3,6 @@ package org.world.meta;
 import arc.*;
 import mindustry.world.meta.*;
 
-import static mindustry.content.Blocks.*;
-
 public class OrgAttribute {
   public static final Attribute resin = Attribute.add("resin");
 }
